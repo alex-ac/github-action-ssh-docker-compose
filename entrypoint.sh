b3115ffece706b5ae2a3617d823a745d9296c20d
Aleksandr Derbenev <ya.alex-ac@yandex.com>
2020-11-18 11:05:36 +0000

    Update remote command. Transfer archive.

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,16 +5,20 @@ log() {
   echo ">> [local]" $@
 }
 
+cleanup() {
+  set +e
+  log "Killing ssh agent."
+  ssh-agent -k
+  log "Removing workspace archive."
+  rm -f /tmp/workspace.tar.bz2
+}
+trap cleanup EXIT
+
 log "Packing workspace into archive to transfer onto remote machine."
 tar cjvf /tmp/workspace.tar.bz2 --exclude .git .
 
 log "Launching ssh agent."
 eval `ssh-agent -s`
-cleanup() {
-  log "Killing ssh agent."
-  ssh-agent -k
-}
-trap cleanup EXIT
 
 ssh-add <(echo "$SSH_PRIVATE_KEY")
 
@@ -22,7 +26,7 @@ remote_command=<< EOF
 set -e
 
 log() {
-  echo ">> [remote]" $@
+  echo ">> [remote]" \$@
 }
 
 cleanup() {
@@ -31,20 +35,21 @@ cleanup() {
 }
 
 log "Creating workspace directory..."
-mkdir "$$HOME/workspace"
+mkdir "\$HOME/workspace"
 trap cleanup EXIT
 
 log "Unpacking workspace..."
-tar -C "$$HOME/workspace" xjv
+tar -C "\$HOME/workspace" xjv
 
 log "Launching docker-compose..."
 COMPOSE_PROJECT="$DOCKER_COMPOSE_PREFIX"
 COMPOSE_FILENAME="$DOCKER_COMPOSE_FILENAME"
-cd $$HOME/workspace
+cd \$HOME/workspace
 docker-compose up -d
 EOF
 
 echo ">> [local] Connecting to remote host."
 ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null \
   "$SSH_USER@$SSH_HOST" -p "$SSH_PORT" \
-  "$remote_command"
+  "$remote_command" \
+  < /tmp/workspace.tar.bz2
